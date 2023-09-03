e57e638d8f34bc6b5a2f073ed457bf9846c2df3b
Keiner Peñaranda <keinerjpp@gmail.com>
2023-09-03 17:16:52 -0500

    change componet api/product/[id]/route

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from "next/server";
-import { NextApiRequest } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import {pool} from '../../../../database/config';
 import {Product} from '../../../../types';
 
@@ -9,7 +8,7 @@ interface Params {
   }
 }
 
-export async function GET(request: NextApiRequest, {params}: Params) {
+export async function GET(request: NextRequest, {params}: Params) {
   try {
     const connection = await pool.getConnection();
     const product = await connection.query(`SELECT * FROM products as pro INNER JOIN images as img WHERE pro.id_image = img.id AND pro.id = ${params.id}`)
