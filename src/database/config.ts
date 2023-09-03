97f716013aea0300b158b7de7898a30c8ea8d1e7
Keiner Peñaranda <keinerjpp@gmail.com>
2023-09-03 18:28:21 -0500

    add config database remote

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,9 +1,9 @@
 import {createPool} from 'mysql2/promise';
 
 export const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'sql10.freesqldatabase.com',
+  user: 'sql10644241',
+  password: '2NU1HLxzae',
   port: 3306,
-  database: 'serviciostic'
+  database: 'sql10644241'
 })
