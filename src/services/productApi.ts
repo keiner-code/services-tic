03a89e225dd92fc8211a10a9e3a3849721d28867
Keiner Peñaranda <keinerjpp@gmail.com>
2023-09-04 21:07:10 -0500

    add cors

--- src/services/productApi.ts
+++ src/services/productApi.ts
@@ -5,7 +5,16 @@ export const productApi = createApi({
   reducerPath: 'productApi',
   refetchOnFocus: true,
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NODE_ENV === 'production' ? "https://services-oh0i8yrlk-keiner-code.vercel.app/api/" : "http://localhost:3000/api/",
+    baseUrl: process.env.NODE_ENV === 'production' ? "https://services-tic.vercel.app/api/" : "http://localhost:3000/api/",
+    credentials: "same-origin", 
+    prepareHeaders: (headers) => {
+        //const accessToken = localStorage.getItem("token");
+        //if (accessToken) {
+            headers.set('Access-Control-Allow-Origin', 'https://services-tic.vercel.app');
+            headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        //}
+        return headers;
+    },
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query<Product[], null>({
